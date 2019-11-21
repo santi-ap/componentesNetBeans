921d18fbf6dec656a8879b4f63ee69dc0d080d7f
jairf <jairf@LAPTOP-SF1TB5FV>
2019-11-20 22:42:17 -0600

    just testing

--- ComponentesWeb/src/java/com/ulatina/pagesController/ViewFormAnswerController.java
+++ ComponentesWeb/src/java/com/ulatina/pagesController/ViewFormAnswerController.java
@@ -14,30 +14,39 @@ import javax.faces.context.FacesContext;
  *
  * @author Asus
  */
-@ManagedBean(name="viewFormAnswerController")
+@ManagedBean(name = "viewFormAnswerController")
 @SessionScoped
 public class ViewFormAnswerController {
-    private int formId;
 
-    public int getFormId() 
-    {
-        return formId;
-    }
+    private int formId
+    private boolean value1;
 
-    public void setFormId(int formId) 
-    {
-        this.formId = formId;
-    }
-    
     /**
-     *  this method takes the info sent through the URL
-     *  and sets it to the local variable formId
+     * this method takes the info sent through the URL and sets it to the local
+     * variable formId
      */
-    public void init()
-    {
+    public void init() {
         FacesContext context = FacesContext.getCurrentInstance();
-        Map<String, String> paramMap=context.getExternalContext().getRequestParameterMap();
+        Map<String, String> paramMap = context.getExternalContext().getRequestParameterMap();
         String projectId = paramMap.get("id");
         this.setFormId(Integer.parseInt(projectId));
     }
+
+        
+    public boolean isValue1() {
+        return value1;
+    }
+
+    public void setValue1(boolean value1) {
+        this.value1 = value1;
+    }
+
+    public int getFormId() {
+        return formId;
+    }
+
+    public void setFormId(int formId) {
+        this.formId = formId;
+    }
+
 }
