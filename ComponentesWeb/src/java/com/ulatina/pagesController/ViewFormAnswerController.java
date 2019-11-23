b29e8212058807627264f349ca59cebc5341b630
David Alfonso <code.david99@hotmail.com>
2019-11-22 19:06:13 -0600

    Finished show results by percentages

--- ComponentesWeb/src/java/com/ulatina/pagesController/ViewFormAnswerController.java
+++ ComponentesWeb/src/java/com/ulatina/pagesController/ViewFormAnswerController.java
@@ -78,40 +78,43 @@ public class ViewFormAnswerController {
         System.out.println(this.form.getTitle());
     }
 
-     /**
-     * this method takes a question and it goes through the answers and calculates the percentages of the results int he case of mult o single choice
-     * 
+    /**
+     * this method takes a question and it goes through the answers and
+     * calculates the percentages of the results int he case of mult o single
+     * choice
+     *
      */
-    public Set<String> generateResults(Question q) {
+    public Set<String> generateResults(Question q) { // gets the from from myFormsController
         Set<String> resultList = new HashSet<>();
 
-        if (q.getType().getId() == 1 || q.getType().getId() == 2) {
-        for (Choice c : q.getChoiceList()) {
+        if (q.getType().getId() == 1 || q.getType().getId() == 2) { // if the question type is multiple choice or single choice it does the stuff to calculate the percentages
+            for (Choice c : q.getChoiceList()) {
 
-            
                 double amountOfAnswers = 0;
                 String result;
 
-                for (Answer a : q.getAnswerList()) {
-                    if (a.getChoice().getId() == c.getId()) {
-                        amountOfAnswers = amountOfAnswers + 1;
+                if (q.getAnswerList().size()==0) {
+                    resultList.add("No Answers");
+                } else {
+                    for (Answer a : q.getAnswerList()) {  // calculates how many people chose a certain asnwer
+                        if (a.getChoice().getId() == c.getId()) {
+                            amountOfAnswers = amountOfAnswers + 1;
+                        }
                     }
+
+                    this.percentage = ((amountOfAnswers / this.numAns) * 100);
+                    result = c.getChoice() + ": " + this.percentage + "%";
+                    resultList.add(result);
                 }
-
-                this.percentage = ((amountOfAnswers / this.numAns) * 100);
-                result = c.getChoice() + ": " + this.percentage + "%";
-                resultList.add(result);
-
-            } 
-        }else { 
-               
-                    for (Answer a: q.getAnswerList()){
-                        resultList.add(a.getAnswer());
-                    }
-                    
-                
             }
-        
+        } else {
+
+            for (Answer a : q.getAnswerList()) {
+                resultList.add(a.getAnswer());
+            }
+
+        }
+
         return resultList;
     }
 
